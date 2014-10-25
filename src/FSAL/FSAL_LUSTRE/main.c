e608c98ed1e1acb94603dd2620799e02b91e57bf
Marc Eshel <eshel@us.ibm.com>
2014-10-24 17:03:35 -0700

    Add the option to have a Ganesha server that is only DS or MDS.
    Signed-off-by: Marc Eshel <eshel@us.ibm.com>

--- src/FSAL/FSAL_LUSTRE/main.c
+++ src/FSAL/FSAL_LUSTRE/main.c
@@ -65,7 +65,8 @@ static struct fsal_staticfsinfo_t lustre_info = {
 	.supported_attrs = LUSTRE_SUPPORTED_ATTRIBUTES,
 	.maxread = FSAL_MAXIOSIZE,
 	.maxwrite = FSAL_MAXIOSIZE,
-	.pnfs_file = true,
+	.pnfs_mds_file = true,
+	.pnfs_ds_file = true,
 };
 
 static void *dataserver_init(void *link_mem, void *self_struct)
--- src/FSAL/commonlib.c
+++ src/FSAL/commonlib.c
@@ -393,8 +393,10 @@ void display_fsinfo(struct fsal_staticfsinfo_t *info)
 		 info->share_support_owner);
 	LogDebug(COMPONENT_FSAL, "  delegations = %d  ",
 		 info->delegations);
-	LogDebug(COMPONENT_FSAL, "  pnfs_file = %d  ",
-		 info->pnfs_file);
+	LogDebug(COMPONENT_FSAL, "  pnfs_mds_file = %d  ",
+		 info->pnfs_mds_file);
+	LogDebug(COMPONENT_FSAL, "  pnfs_ds_file = %d  ",
+		 info->pnfs_ds_file);
 	LogDebug(COMPONENT_FSAL, "  fsal_trace = %d  ",
 		 info->fsal_trace);
 	LogDebug(COMPONENT_FSAL, "}");
